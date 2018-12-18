45658e442d7a0ea8dc85d3eafa0de2575a84c343
Chanda Grayson <chandagrayson@gmail.com>
2018-12-18 16:14:14 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 4, 'M', 'pink', 'chill', '1'),
+('Rowdy', 7, 'M', 'tan', 'goofy', '1'),
+('Tabitha', 4, 'F', 'pink', 'chill', '1'),
+('Sergeant Brown', 4, 'M', 'brown', 'chill', '1'),
+('Melissa', 9, 'F', 'pink', 'chill', '1'),
+('Grinch', 4, 'M', 'pink', 'crazy', '1'),
+('Wendy', 2, 'F', 'blue', 'chill', '1'),
+(NULL, 4, 'M', 'pink', 'nutso', '1');
+
+
+
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
